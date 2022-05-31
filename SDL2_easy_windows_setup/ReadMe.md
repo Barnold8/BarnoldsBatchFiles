10cd8a2b0642361762b90dbfd9238472031a0116
Brandon Wright <psywb7@nottingham.ac.uk>
2022-05-31 01:37:17 +0100

    Im officially done with this mistake of a project

--- SDL2_easy_windows_setup/ReadMe.md
+++ SDL2_easy_windows_setup/ReadMe.md
@@ -3,12 +3,14 @@
 
 __What this is__
 
-This is a simple .bat file that aims to make the process of getting SDL less painful and less daunting.
+This is a simple .bat (kind of) file that aims to make the process of getting SDL less painful and less daunting.
+
+Unfortunately windows is annoying to make automation scripts for in its own native scripting language "batch". I have not tried powershell as of yet but it could be a lot better. I would personally follow this following guide: [SLD2](https://www.matsson.com/prog/sdl2-mingw-w64-tutorial.php#:~:text=Go%20to%20the%20SDL2%20download,library%20for%20Windows%20using%20MinGW.&text=After%20extracting%20the%20contents%20using,bit%20version%20of%20the%20library.), rather than using this batch file since making this has become very messy and im sick of testing it and fixes all the stupid ambiguities that windows produces on a whim. 
 
 ----
 
 __What you need!__
 
- **17063 Version of windows or later needed for tar**
+**17063 Version of windows or later needed for tar**
 
 **Windows XP or later for bitsadmin**
--- SDL2_easy_windows_setup/SDL2_SETUP_WIN.bat
+++ SDL2_easy_windows_setup/SDL2_SETUP_WIN.bat
@@ -1,17 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
+net session >nul 2>&1
+    IF %ERRORLEVEL% EQU 0 (
+        title Running as admin!
+    ) ELSE (
+        title Not running as admin ^(May cause problems ^if you want to automate PATH^)
+        )
 
-@REM start sysdm.cpl
-@REM setx sets environment variable 
-@REM setx TEST 12
 
+echo Since this is a simple batch file. Error checking is quite difficult. 
+echo If you have any errors be sure to close this down and launch the file again. 
+echo It may also be a good idea to remove any files/directories in case for naming ambiguities.
+pause
 cls
 
-set /p minGW=" Do you have minGW installed? Y/N (NOTE: it is needed for compiling code) "
+set /p minGW_Install=" MinGW is needed for compiling C/C++ code, do you wish to install it? (its mandatory for SDL2 development) Y/N "
 
-if %minGW% == N goto MINGW 
-if %minGW% == n goto MINGW 
+if %minGW_Install% == y goto MINGW 
+if %minGW_Install% == Y goto MINGW 
 
 echo "Grabbing SDL2 from the internet!"
 
@@ -20,27 +27,36 @@ goto SDL2_INSTALL
 :MINGW
     set per=mod
     set per=!per:mod=%%!
-    set downloadLink="https://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/8.1.0/threads-win32/seh/x86_64-8.1.0-release-win32-seh-rt_v6-rev0.7z?ts=gAAAAABijRezKL48CgiTkLOY-vCFQxM7TExCTHlh6ir7VNVCE8_kl6kXlw-_HSF71kVgRK4n4PksPKfumweJU6FDmAcV66bSqg%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fmingw-w64%2Ffiles%2FToolchains%2520targetting%2520Win64%2FPersonal%2520Builds%2Fmingw-builds%2F8.1.0%2Fthreads-win32%2Fseh%2Fx86_64-8.1.0-release-win32-seh-rt_v6-rev0.7z%2Fdownload"
+    set downloadLink="https://downloads.sourceforge.net/project/mingw-w64/Toolchains%per%20targetting%per%20Win64/Personal%per%20Builds/mingw-builds/7.3.0/threads-win32/seh/x86_64-7.3.0-release-win32-seh-rt_v5-rev0.7z?ts=gAAAAABijTB5nhqE6lvsq3lsbwVsfC4kMI6_7l19IX0uiSOSOMZH7x0N9GkNUDYiyQVLt39OCmhLaOCwo9bJUKc3_k4kPpHwrQ%per%3D%per%3D&r=https%per%3A%per%2F%per%2Fsourceforge.net%per%2Fprojects%per%2Fmingw-w64%per%2Ffiles%per%2FToolchains%per%2520targetting%per%2520Win64%per%2FPersonal%per%2520Builds%per%2Fmingw-builds%per%2F7.3.0%per%2Fthreads-win32%per%2Fseh%per%2Fx86_64-7.3.0-release-win32-seh-rt_v5-rev0.7z%per%2Fdownload"
 
     bitsadmin /Reset
-    bitsadmin /create "MinGw_Install_Process"
-    ::bitsadmin /SETPROXYSETTINGS "MinGw_Install_Process" NO_PROXY
+    bitsadmin /create "MinGwInstallProcess"
 
-    bitsadmin /transfer MinGw_Install_Process /download /DYNAMIC /priority high %downloadLink% "%CD%\MingGW_Setup.exe"
-
-    bitsadmin /resume MinGw_Install_Process
-    bitsadmin /complete MinGw_Install_Process
-
+    ::bitsadmin /SETPROXYSETTINGS "MinGwInstallProcess" NO_PROXY
     cls
+    net session >nul 2>&1
+    IF %ERRORLEVEL% EQU 0 (
+        set /p adminPath=" You are running this as admin, this means that the download will go to system32. Please enter the path where you want minGW to go, else leave a dot to keep it in system32 (not recommended) "
+        if adminPath == . bitsadmin /transfer MinGwInstallProcess /download /DYNAMIC /priority high %downloadLink% "%CD%\MingGW_Setup.7z"
+        if adminPath == . goto MINGW2
+        bitsadmin /transfer MinGwInstallProcess /download /DYNAMIC /priority high %downloadLink% "%USERPROFILE%\MingGW_Setup.7z" 
+        
+    ) ELSE (
+        bitsadmin /transfer MinGwInstallProcess /download /DYNAMIC /priority high %downloadLink% "%CD%\MingGW_Setup.7z"
+        )
 
-    echo Please run the setup tool, or cancel it, to continue. 
+:MINGW2
 
-    start MingGW_Setup.exe
-
-    cls
-
-
-
+    bitsadmin /resume MinGwInstallProcess
+    bitsadmin /complete MinGwInstallProcess
+    mkdir %adminPath%
+    move "%USERPROFILE%\MingGW_Setup.7z" "%adminPath%\MingGW_Setup.7z"
+    start %adminPath%
+    echo Please unzip the mingGW zip archive before continuing... ^(This is needed^)
+    pause
+    if adminPath == . setx /M path "%path%;%CD%\MINGW\mingw\mingw64"
+    if adminPath == . gotO SDL2_INSTALL
+    setx /M path "%path%;%adminPath%\mingw64\bin"
 
 :SDL2_INSTALL
 
@@ -49,48 +65,54 @@ goto SDL2_INSTALL
     set downloadLink="https://www.libsdl.org/release/SDL2-devel-2.0.22-mingw.tar.gz"
 
     bitsadmin /Reset
-    bitsadmin /create "SDL2_Install_Process"
-    ::bitsadmin /SETPROXYSETTINGS "SDL2_Install_Process" NO_PROXY
-
-    bitsadmin /transfer SDL2_Install_Process /download /DYNAMIC /priority high %downloadLink% "%CD%\SDL2-devel-2.0.22-mingw.tar.gz"
-
-    bitsadmin /resume SDL2_Install_Process
-    bitsadmin /complete SDL2_Install_Process
-
+    bitsadmin /create "SDL2InstallProcess"
+    ::bitsadmin /SETPROXYSETTINGS "SDL2InstallProcess" NO_PROXY
+    bitsadmin /transfer SDL2InstallProcess /download /DYNAMIC /priority high %downloadLink% "%USERPROFILE%\Downloads\SDL2-devel-2.0.22-mingw.tar.gz"
+    bitsadmin /resume SDL2InstallProcess
+    bitsadmin /complete SDL2InstallProcess
+ 
     cls
 
     
 :ExtractFiles 
 
-    tar -xf SDL2-devel-2.0.22-mingw.tar.gz
-    del SDL2-devel-2.0.22-mingw.tar.gz
+
+    net session >nul 2>&1
+    IF %ERRORLEVEL% EQU 0 (
+            echo tar -xf SDL2-devel-2.0.22-mingw.tar.gz >> tarEXT.bat
+            echo del SDL2-devel-2.0.22-mingw.tar.gz >> tarEXT.bat
+            echo del tarEXT.bat >> tarEXT.bat
+            move tarEXT.bat "%USERPROFILE%\Downloads"
+            echo run 'tarEXT.BAT' to extract the tar archive.
+            cmd /c "start %USERPROFILE%\Downloads"
+            
+    ) ELSE (
+        
+        tar -xf SDL2-devel-2.0.22-mingw.tar.gz
+        del SDL2-devel-2.0.22-mingw.tar.gz
+
+    )
+
     
-:Code_FLDR_Setup:
 
-    set /p dir=" Please enter the exact directory you want the SDL library to reside in "
-
-    if %dir% == . goto Leave_as_is
-
-
-:Move_Process
-
-    xcopy "SDL2-2.0.22/x86_64-w64-mingw32" %dir%
+pause
+cls
+echo if you want to keep the entire SDL folder, please make a copy and place it somewhere else since this script will move the x86_64 bin to the code folder
+pause
+cls
 
 
-:Leave_as_is
-    
-    set /p Code_dir=" Please enter the exact directory you want your code project. Or alternatively you can type . to abort thus far "
-    mkdir %Code_dir%
-    if %Code_dir% == . exit
-
-    echo "To allow this process to write, please write D when you see '(F = file, D = directory)' "
-
-    xcopy "%dir%/bin" "%Code_dir%/SDL"
-    
+mkdir "%USERPROFILE%\Downloads\SLD2_Code_Sample"
+move  "%USERPROFILE%\Downloads\SDL2-2.0.22\x86_64-w64-mingw32" "%USERPROFILE%\Downloads\SLD2_Code_Sample"
+xcopy "%USERPROFILE%\Downloads\SLD2_Code_Sample\x86_64-w64-mingw32\bin" "%USERPROFILE%\Downloads\SLD2_Code_Sample"
+ren "%USERPROFILE%\Downloads\SLD2_Code_Sample\x86_64-w64-mingw32" SDL2
 
 :write_code
-    
-    move "%Code_dir%/SDL/SDL2.dll" %Code_dir%
+
+
+    set per2=mod
+    set per2=!per2:mod=!!
+
     
     echo #include ^<stdio.h^> >> main.cpp
     echo #include ^<SDL2/SDL.h^> >> main.cpp
@@ -98,11 +120,147 @@ goto SDL2_INSTALL
     echo int main^(int argc, char* argv^[^]^)^{  >> main.cpp
     echo ^/* Initializes the timer, audio, video, joystick,  >> main.cpp
     echo haptic, gamecontroller and events subsystems *^/ >> main.cpp
-    echo if ^(SDL_Init^(SDL_INIT_EVERYTHING^) ^!= 0^)^{   >> main.cpp
-    echo     printf^("Error initializing SDL: ^%s\n", SDL_GetError^(^)^);    >> main.cpp
+    echo if ^(SDL_Init^(SDL_INIT_EVERYTHING^) ^^!= 0^)^{   >> main.cpp
+    echo     printf^("Error initializing SDL: %per%s\n", SDL_GetError^(^)^);    >> main.cpp
     echo     return 0;^} >> main.cpp
     echo printf^("SDL successfully initialized!\n"^); >> main.cpp
     echo SDL_Quit^(^);    >> main.cpp
     echo return 0;  >> main.cpp
     echo }   >> main.cpp
-    move main.cpp %Code_dir%
+    move main.cpp "%USERPROFILE%\Downloads\SLD2_Code_Sample"
+
+    echo g++ main.cpp -ISDL2/include -LSDL2/lib -Wall -lmingw32 -lSDL2main -lSDL2 -o main >> compile.bat
+    echo pause >> compile.bat
+
+    move compile.bat "%USERPROFILE%\Downloads\SLD2_Code_Sample"
+
+    
+
+        
+    echo #include ^<stdio.h^>          >> sampleGame.cpp
+    echo #include ^<stdbool.h^>          >> sampleGame.cpp
+    echo #include ^<SDL2/SDL.h^>          >> sampleGame.cpp
+    echo #define WIDTH 640          >> sampleGame.cpp
+    echo #define HEIGHT 480          >> sampleGame.cpp
+    echo #define SIZE 200          >> sampleGame.cpp
+    echo #define SPEED 600          >> sampleGame.cpp
+    echo #define GRAVITY 60          >> sampleGame.cpp
+    echo #define FPS 60          >> sampleGame.cpp
+    echo #define JUMP -1200          >> sampleGame.cpp
+
+    echo int main^(int argc, char* argv^[^]^)^{          >> sampleGame.cpp
+    echo ^/* Initializes the timer, audio, video, joystick,          >> sampleGame.cpp
+    echo haptic, gamecontroller and events subsystems *^/          >> sampleGame.cpp
+    echo if ^(SDL_Init^(SDL_INIT_EVERYTHING^) ^^!= 0^)^{          >> sampleGame.cpp
+    echo     printf^("Error initializing SDL: %per%s\n", SDL_GetError^(^)^);          >> sampleGame.cpp
+    echo     return 0;^}          >> sampleGame.cpp
+    echo  ^/* Create a window *^/          >> sampleGame.cpp
+    echo SDL_Window* wind = SDL_CreateWindow^("Hello Platformer!",          >> sampleGame.cpp
+    echo                                     SDL_WINDOWPOS_CENTERED,          >> sampleGame.cpp
+    echo                                     SDL_WINDOWPOS_CENTERED,          >> sampleGame.cpp
+    echo                                     WIDTH, HEIGHT, 0^);          >> sampleGame.cpp
+    echo if ^(!wind^)          >> sampleGame.cpp
+    echo ^{          >> sampleGame.cpp
+    echo     printf^("Error creating window: %per%s\n", SDL_GetError^(^)^);          >> sampleGame.cpp
+    echo     SDL_Quit^(^);          >> sampleGame.cpp          >> sampleGame.cpp
+    echo     return 0;          >> sampleGame.cpp          >> sampleGame.cpp
+    echo ^}          >> sampleGame.cpp          >> sampleGame.cpp
+    echo ^/* Create a renderer *^/          >> sampleGame.cpp
+    echo Uint32 render_flags = SDL_RENDERER_ACCELERATED ^| SDL_RENDERER_PRESENTVSYNC;          >> sampleGame.cpp
+    echo SDL_Renderer* rend = SDL_CreateRenderer^(wind, -1, render_flags^);          >> sampleGame.cpp
+    echo if ^(^^!rend^)^{          >> sampleGame.cpp
+    echo    printf^("Error creating renderer: %per%s\n", SDL_GetError^(^)^);          >> sampleGame.cpp
+    echo     SDL_DestroyWindow^(wind^);          >> sampleGame.cpp
+    echo     SDL_Quit^(^);          >> sampleGame.cpp
+    echo     return 0;^}          >> sampleGame.cpp
+    echo /* Main loop */          >> sampleGame.cpp
+    echo bool running = true, jump_pressed = false, can_jump = true,          >> sampleGame.cpp
+    echo                 left_pressed = false, right_pressed = false;          >> sampleGame.cpp
+    echo float x_pos = ^(WIDTH-SIZE^)/2, y_pos = ^(HEIGHT-SIZE^)/2, x_vel = 0, y_vel = 0;          >> sampleGame.cpp
+    echo SDL_Rect rect = ^{^(int^) x_pos, ^(int^) y_pos, SIZE, SIZE^};          >> sampleGame.cpp
+    echo SDL_Event event;          >> sampleGame.cpp
+    echo while ^(running^)^{          >> sampleGame.cpp
+    echo     /* Process events */          >> sampleGame.cpp
+    echo     while ^(SDL_PollEvent^(^&event^)^)^{          >> sampleGame.cpp
+    echo     switch ^(event.type^)^{          >> sampleGame.cpp
+    echo         case SDL_QUIT:          >> sampleGame.cpp
+    echo        running = false;          >> sampleGame.cpp
+    echo        break;          >> sampleGame.cpp
+    echo         case SDL_KEYDOWN:          >> sampleGame.cpp
+    echo         switch ^(event.key.keysym.scancode^)^{          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_SPACE:          >> sampleGame.cpp
+    echo             jump_pressed = true;          >> sampleGame.cpp
+    echo             break;          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_A:          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_LEFT:          >> sampleGame.cpp
+    echo             left_pressed = true;          >> sampleGame.cpp
+    echo             break;          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_D:          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_RIGHT:          >> sampleGame.cpp
+    echo             right_pressed = true;          >> sampleGame.cpp
+    echo             break;          >> sampleGame.cpp
+    echo             default:          >> sampleGame.cpp
+    echo             break;^}          >> sampleGame.cpp
+    echo         break;          >> sampleGame.cpp
+    echo         case SDL_KEYUP:          >> sampleGame.cpp
+    echo         switch (event.key.keysym.scancode^)^{          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_SPACE:          >> sampleGame.cpp
+    echo             jump_pressed = false;          >> sampleGame.cpp
+    echo             break;          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_A:          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_LEFT:          >> sampleGame.cpp
+    echo             left_pressed = false;          >> sampleGame.cpp
+    echo             break;          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_D:          >> sampleGame.cpp
+    echo             case SDL_SCANCODE_RIGHT:          >> sampleGame.cpp
+    echo             right_pressed = false;          >> sampleGame.cpp
+    echo             break;          >> sampleGame.cpp
+    echo             default:          >> sampleGame.cpp
+    echo             break;^}          >> sampleGame.cpp
+    echo         break;          >> sampleGame.cpp
+    echo         default:          >> sampleGame.cpp
+    echo         break;^}^}          >> sampleGame.cpp
+    echo     /* Clear screen */          >> sampleGame.cpp
+    echo     SDL_SetRenderDrawColor^(rend, 0, 0, 0, 255^);          >> sampleGame.cpp
+    echo     SDL_RenderClear^(rend^);          >> sampleGame.cpp
+    echo     /* Move the rectangle */          >> sampleGame.cpp
+    echo     x_vel = ^(right_pressed - left_pressed^)*SPEED;          >> sampleGame.cpp
+    echo     y_vel += GRAVITY;          >> sampleGame.cpp
+    echo     if ^(jump_pressed ^&^& can_jump^)^{          >> sampleGame.cpp
+    echo     can_jump = false;          >> sampleGame.cpp
+    echo     y_vel = JUMP;^}          >> sampleGame.cpp
+    echo     x_pos += x_vel / 60;          >> sampleGame.cpp
+    echo     y_pos += y_vel / 60;          >> sampleGame.cpp
+    echo     if ^(x_pos <= 0^)          >> sampleGame.cpp
+    echo     x_pos = 0;          >> sampleGame.cpp
+    echo     if ^(x_pos >= WIDTH - rect.w^)          >> sampleGame.cpp
+    echo     x_pos = WIDTH - rect.w;          >> sampleGame.cpp
+    echo     if ^(y_pos <= 0^)          >> sampleGame.cpp
+    echo     y_pos = 0;          >> sampleGame.cpp
+    echo     if ^(y_pos >= HEIGHT - rect.h^)^{          >> sampleGame.cpp
+    echo     y_vel = 0;          >> sampleGame.cpp
+    echo     y_pos = HEIGHT - rect.h;          >> sampleGame.cpp
+    echo     if ^(!jump_pressed^)          >> sampleGame.cpp
+    echo         can_jump = true;^}          >> sampleGame.cpp
+    echo     rect.x = ^(int^) x_pos;          >> sampleGame.cpp
+    echo     rect.y = ^(int^) y_pos;          >> sampleGame.cpp
+    echo     /* Draw the rectangle */          >> sampleGame.cpp
+    echo     SDL_SetRenderDrawColor^(rend, 255, 0, 0, 255^);          >> sampleGame.cpp
+    echo     SDL_RenderFillRect^(rend, ^&rect^);          >> sampleGame.cpp
+    echo     /* Draw to window and loop */          >> sampleGame.cpp
+    echo     SDL_RenderPresent^(rend^);          >> sampleGame.cpp
+    echo     SDL_Delay^(1000/FPS^);^}          >> sampleGame.cpp
+    echo /* Release resources */          >> sampleGame.cpp
+    echo SDL_DestroyRenderer^(rend^);          >> sampleGame.cpp
+    echo SDL_DestroyWindow^(wind^);          >> sampleGame.cpp
+    echo SDL_Quit^(^);          >> sampleGame.cpp
+    echo return 0;^}          >> sampleGame.cpp
+
+    move sampleGame.cpp "%USERPROFILE%\Downloads\SLD2_Code_Sample"
+
+    echo g++ sampleGame.cpp -ISDL2/include -LSDL2/lib -Wall -lmingw32 -lSDL2main -lSDL2 -o main >> compileSampleGame.bat
+    echo pause >> compileSampleGame.bat
+
+    move compileSampleGame.bat "%USERPROFILE%\Downloads\SLD2_Code_Sample"
+
+    pause
